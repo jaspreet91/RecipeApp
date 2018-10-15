44dffee07830dd1364b8a7ee901dba9dfff8f307
Jaspreet Marwah <jaspreetmarwah@Jaspreets-MacBook-Pro.local>
2018-10-14 23:48:41 -0400

    made changes in recipe-list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[] = [
+    new Recipe('test recipe', 'this is test', 
+    'https://dinnerthendessert.com/wp-content/uploads/2016/12/Slow-Cooker-Indian-Butter-Chicken-2.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
